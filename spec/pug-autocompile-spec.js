92ab3d6d44975da78938bc659f40456784101d8b
Vladimir <dopustimvladimir@gmail.com>
2021-12-15 22:31:08 +0600

    Reformat code

--- spec/pug-autocompile-spec.js
+++ spec/pug-autocompile-spec.js
@@ -1,89 +1,91 @@
-'use babel';
+"use babel"
 
-import fs from 'fs';
-import path from 'path';
+import fs from "fs"
+import path from "path"
 
-import PugAutocompile from '../lib/pug-autocompile';
+import PugAutocompile from "../lib/pug-autocompile"
 
-const atom = global.atom;
+const atom = global.atom
 
-const path2pug = path.resolve(__dirname, '../test/index.pug');
-const path2html = path.resolve(__dirname, '../test/build/index.html');
-const path2html_ex = path.resolve(__dirname, '../test/build_ex/index.html');
+const path2Pug = path.resolve(__dirname, "../test/index.pug")
+const path2Html = path.resolve(__dirname, "../test/build/index.html")
+const path2HtmlEx = path.resolve(__dirname, "../test/build_ex/index.html")
 
-const compileFileCommand = 'pug-autocompile:compile-file';
-const compileDirectCommand = 'pug-autocompile:compile-direct';
+const compileFileCommand = "pug-autocompile:compile-file"
+const compileDirectCommand = "pug-autocompile:compile-direct"
 
-describe('Package "pug-autocompile"', () => {
+// eslint-disable-next-line max-lines-per-function
+describe("Package \"pug-autocompile\"", () => {
 
     beforeEach(() => {
         waitsForPromise(() => {
-            return atom.packages.activatePackage('pug-autocompile');
-        });
-    });
+            return atom.packages.activatePackage("pug-autocompile")
+        })
+    })
 
-    it('should be activated', () => {
-        expect(atom.packages.isPackageActive('pug-autocompile')).toBe(true);
-    });
+    it("should be activated", () => {
+        expect(atom.packages.isPackageActive("pug-autocompile")).toBe(true)
+    })
 
-    describe('File "index.pug"', () => {
+    // eslint-disable-next-line max-lines-per-function
+    describe("File \"index.pug\"", () => {
 
-        let editor, editorElement, compiled, expected;
+        let editor, editorElement, compiled, expected
 
         beforeEach(() => {
             waitsForPromise(() => {
-                return atom.workspace.open(path2pug).then((ed) => {
-                    editor = ed;
-                    editorElement = atom.views.getView(editor);
-                });
-            });
-        });
+                return atom.workspace.open(path2Pug).then((ed) => {
+                    editor = ed
+                    editorElement = atom.views.getView(editor)
+                })
+            })
+        })
 
-        it('should be opened', () => {
-            expect(PugAutocompile.isPugFile(editor.getPath())).toBe(true);
-        });
+        it("should be opened", () => {
+            expect(PugAutocompile.isPugFile(editor.getPath())).toBe(true)
+        })
 
-        it('should be properly compiled to "index.html" via command "compile-file"', () => {
+        it("should be properly compiled to \"index.html\" via command \"compile-file\"", () => {
 
-            spyOn(PugAutocompile, 'showSuccess');
+            spyOn(PugAutocompile, "showSuccess")
 
             runs(() => {
-                atom.commands.dispatch(editorElement, compileFileCommand);
-            });
+                atom.commands.dispatch(editorElement, compileFileCommand)
+            })
 
             waitsFor(() => {
-                return PugAutocompile.showSuccess.callCount > 0;
-            });
+                return PugAutocompile.showSuccess.callCount > 0
+            })
 
             runs(() => {
-                compiled = fs.readFileSync(path2html, 'utf8');
-                expected = fs.readFileSync(path2html_ex, 'utf8');
-                expect(compiled).toBe(expected);
-            });
-        });
+                compiled = fs.readFileSync(path2Html, "utf8")
+                expected = fs.readFileSync(path2HtmlEx, "utf8")
+                expect(compiled).toBe(expected)
+            })
+        })
 
-        describe('All selected content"', () => {
+        describe("All selected content\"", () => {
 
-            it('should be properly replaced with HTML via command "compile-direct"', () => {
+            it("should be properly replaced with HTML via command \"compile-direct\"", () => {
 
-                spyOn(PugAutocompile, 'showSuccess');
+                spyOn(PugAutocompile, "showSuccess")
 
                 runs(() => {
-                    editor.selectAll();
-                    atom.commands.dispatch(editorElement, compileDirectCommand);
-                });
+                    editor.selectAll()
+                    atom.commands.dispatch(editorElement, compileDirectCommand)
+                })
 
                 waitsFor(() => {
-                    return PugAutocompile.showSuccess.callCount > 0;
-                });
+                    return PugAutocompile.showSuccess.callCount > 0
+                })
 
                 runs(() => {
-                    editor.selectAll();
-                    compiled = editor.getText();
-                    expected = fs.readFileSync(path2html_ex, 'utf8');
-                    expect(compiled).toBe(expected);
-                });
-            });
-        });
-    });
-});
+                    editor.selectAll()
+                    compiled = editor.getText()
+                    expected = fs.readFileSync(path2HtmlEx, "utf8")
+                    expect(compiled).toBe(expected)
+                })
+            })
+        })
+    })
+})
